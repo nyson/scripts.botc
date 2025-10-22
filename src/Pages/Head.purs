45484f4675501bb8080ca83dd3fb925e8f7dd337
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2025-10-22 21:57:03 +0200

    cleanup

--- src/Pages/Head.purs
+++ src/Pages/Head.purs
@@ -18,24 +18,42 @@ import RoleBook as Roles
 
 data PageState = Head | RoleBook | Pamphlets
 
+derive instance eqPageState :: Eq PageState
+
+
 startingPage :: PageState
 startingPage = Pamphlets
 
 page :: forall a. PageState -> Widget HTML a
 page p = do
     newState <- D.div'  
-            [ D.div'
-                [ D.button [P.onClick] [D.text "Head"] $> Head
-                , D.button [P.onClick] [D.text "RoleBook"] $> RoleBook
-                , D.button [P.onClick] [D.text "Pamflets"] $> Pamphlets
-                ]
-            , D.div' 
-                [ case p of
-                    RoleBook -> RoleBookPage.renderBook Roles.roleBook []
-                    Pamphlets -> Pamphlets.page 
-                        (filter (\r -> r.edition == "bmr") 
-                            $ concatMap concat Roles.roleBook)
-                    Head -> D.h2' [D.text "Welcome!"]
-                ]
+        [ header p
+        , D.div' 
+            [ case p of
+                RoleBook -> RoleBookPage.renderBook Roles.roleBook []
+                Pamphlets -> Pamphlets.page 
+                    (filter (\r -> r.edition == "bmr") 
+                        $ concatMap concat Roles.roleBook)
+                Head -> D.h2' [D.text "Welcome!"]
             ]
+        ]
     page newState
+
+header :: PageState -> Widget HTML PageState
+header p = D.ul'
+    [ option p Head "Head"
+    , option p RoleBook "Role Book"
+    , option p Pamphlets "Pamphlets"
+    ]
+    where option st val name = 
+            D.li 
+            [ P.onClick
+            , P.className
+                if st == val 
+                then "option selected"
+                else "option"
+            ] 
+            [D.text name] 
+            $> val
+        
+            
